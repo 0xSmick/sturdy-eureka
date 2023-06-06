3985e172323748acba97f0774da7ab863fe7940c
Sheldon Smickley <sheldon.smickley@gmail.com>
2023-06-06 14:12:07 +0200

    forked and added SEC filing

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import streamlit as st
 
 # Import PDF document loaders...there's other ones as well!
 from langchain.document_loaders import PyPDFLoader
-# Import chroma as the vector store 
+# Import chroma as the vector store
 from langchain.vectorstores import Chroma
 
 # Import vector store stuff
@@ -17,49 +17,49 @@ from langchain.agents.agent_toolkits import (
     VectorStoreInfo
 )
 
-# Set APIkey for OpenAI Service
-# Can sub this out for other LLM providers
-os.environ['OPENAI_API_KEY'] = 'yourapikeyhere'
+api_key = st.text_input('Input your OpenAI API key here')
 
-# Create instance of OpenAI LLM
-llm = OpenAI(temperature=0.1, verbose=True)
+if api_key:
+    os.environ['OPENAI_API_KEY'] = api_key
+    llm = OpenAI(temperature=0, verbose=True)
+    loader = PyPDFLoader('sec.pdf')
+    pages = loader.load_and_split()
+    store = Chroma.from_documents(pages, collection_name='sec-filing')
 
-# Create and load PDF Loader
-loader = PyPDFLoader('annualreport.pdf')
-# Split pages from pdf 
-pages = loader.load_and_split()
-# Load documents into vector database aka ChromaDB
-store = Chroma.from_documents(pages, collection_name='annualreport')
+    # Create vectorstore info object - metadata repo?
+    vectorstore_info = VectorStoreInfo(
+        name="sec-filing",
+        description="an SEC filing against binance as a pdf",
+        vectorstore=store
+    )
+    # Convert the document store into a langchain toolkit
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
 
-# Create vectorstore info object - metadata repo?
-vectorstore_info = VectorStoreInfo(
-    name="annual_report",
-    description="a banking annual report as a pdf",
-    vectorstore=store
-)
-# Convert the document store into a langchain toolkit
-toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
+    # Add the toolkit to an end-to-end LC
+    agent_executor = create_vectorstore_agent(
+        llm=llm,
+        toolkit=toolkit,
+        verbose=True
+    )
 
-# Add the toolkit to an end-to-end LC
-agent_executor = create_vectorstore_agent(
-    llm=llm,
-    toolkit=toolkit,
-    verbose=True
-)
-st.title('🦜🔗 GPT Investment Banker')
-# Create a text input box for the user
-prompt = st.text_input('Input your prompt here')
+    st.title('Ask me anything about the filing!')
+    # Create a text input box for the user
+    prompt = st.text_input('Input your prompt here')
 
-# If the user hits enter
-if prompt:
-    # Then pass the prompt to the LLM
-    response = agent_executor.run(prompt)
-    # ...and write it out to the screen
-    st.write(response)
+    if prompt:
+        # Then pass the prompt to the LLM
+        response = agent_executor.run(prompt)
+        # ...and write it out to the screen
+        st.write(response)
 
-    # With a streamlit expander  
-    with st.expander('Document Similarity Search'):
-        # Find the relevant pages
-        search = store.similarity_search_with_score(prompt) 
-        # Write out the first 
-        st.write(search[0][0].page_content) 
+        # With a streamlit expander
+        with st.expander('Document Similarity Search'):
+            # Find the relevant pages
+            search = store.similarity_search_with_score(prompt)
+            # Write out the first
+            st.write(search[0][0].page_content)
+else:
+    st.warning('Please enter your OpenAI API key to use the app.')
+
+    st.title('GPT SEC Filing')
+    st.warning('Please enter a prompt to ask questions about the PDF.')
